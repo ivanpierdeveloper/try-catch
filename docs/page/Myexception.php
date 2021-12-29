9b60f6d46e9e8dbf4d8fb8e2b4c7d17558179ac3
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-29 15:40:37 +0100

    Delete docs/page directory

--- docs/page/Myexception.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    class Myexception extends Exception {
-        public function message404(string $msg, int $code) {
-            $error = "message " . $this->getMessage() . " passed $msg " . " code " . $this->getCode() . " code passed " . $code . " file " . $this->getFile() . " line " . $this->getLine();
-            return $error;
-        }
-    }
